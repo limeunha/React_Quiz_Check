87d9d024db192570a18296f195b451adca89056d
limeunha <eh1501@naver.com>
2024-10-30 18:05:00 +0900

    수정작업

--- ch03/src/Quiz06.js
+++ ch03/src/Quiz06.js
@@ -2,10 +2,13 @@ import React, { useState } from 'react'
 
 function Quiz06() {
    const [count, setCount] = useState(0)
+
    return (
       <div>
          <p>카운트: {count}</p>
          <button onClick={() => setCount(count + 1)}>1씩 증가</button>
+
+         {/* setCount(count++) 증감연산자는 count값 자체를 바꾸므로 사용할수X->count는 const로 선언돠어있음 -> count는 setCount로 */}
       </div>
    )
 }
