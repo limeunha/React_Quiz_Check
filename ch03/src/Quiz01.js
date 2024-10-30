545082f071a1d3a21b8ebc76ac91200827898e14
limeunha <eh1501@naver.com>
2024-10-30 13:18:28 +0900

    수정작업

--- ch03/src/Quiz01.js
+++ ch03/src/Quiz01.js
@@ -1,16 +1,15 @@
-function Quiz01() { 
-    return (
-        <div>
-            <h2>학생정보</h2>
-            <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
-        </div>
-    )
+function Quiz01(props) {
+   return (
+      <div>
+         <h2>학생정보</h2>
+         <ul>
+            <li>이름 : {props.name}</li>
+            <li>나이 : {props.age}세</li>
+            <li>핸드폰 번호 : {props.phone}</li>
+            <li>학과 : {props.major}</li>
+         </ul>
+      </div>
+   )
 }
-
 
 export default Quiz01
--- ch03/src/Quiz05.js
+++ ch03/src/Quiz05.js
@@ -1,18 +1,28 @@
-function Quiz05() {
-    return (
-        <div>
-            <h2></h2>
-            <h3></h3>
-            <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
-        </div>
-    )
+import PropTypes from 'prop-types'
+
+function Quiz05(props) {
+   const { major, start, grade } = props
+
+   return (
+      <div>
+         <h2>{major}성적</h2>
+         <h3>개강여부 : {start.toString()}</h3>
+         <ul>
+            <li>{grade[0]}</li>
+            <li>{grade[1]}</li>
+            <li>{grade[2]}</li>
+            <li>{grade[3]}</li>
+            <li>{grade[4]}</li>
+            <li>{grade[5]}</li>
+         </ul>
+      </div>
+   )
 }
 
-export default Quiz05;
+Quiz05.propTypes = {
+   major: PropTypes.string.isRequired,
+   grade: PropTypes.array.isRequired,
+   start: PropTypes.bool,
+}
+
+export default Quiz05
